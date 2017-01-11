4505607c3fc9f5beed916b5b6b5d8ea04c05ef1a
Jairo A. Román Alvia <lex-roman@hotmail.com>
2017-01-10 22:34:40 -0500

    MostrarMenu
    Muestra un menú utilizando los mensajes por pantalla, hay dos formas en
    el código en las que se obtiene el mismo resultado pero la estética es
    considerablemente diferente, aunque considero que sirve para entender la
    interacción de algunos comandos.

---
+++ (ejercicio2)CursoC02.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursoc02;
+
+/**
+ *Programa: 02
+ * Descripcion: programa de un menu
+ * @author Jairo Roman
+ * Fecha: Diciembre 2, 2014
+ */
+public class CursoC02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       //Programa pedido por profesor bajo terminos segùn la clase
+        System.out.println("======Menù Manejo Pila======");
+        System.out.println("=                          =");
+        System.out.println("= 1- Insertar el elemento  =");
+        System.out.println("= 2- Elmiminar elemento    =");
+        System.out.println("= 3- Buscar elemento       =");
+        System.out.println("= 4- Imprimir pila         =");
+        System.out.println("= 5- Salir                 =");
+        System.out.println("=                          =");
+        System.out.println("============================");
+        System.out.println("Opciòn:");
+        //Segunda opciòn personal hecha para ver el funcionamiento de los caracteres (\n)
+        System.out.println("======Menù Manejo Pila====== \n=                          = \n= 1- Insertar el elemento  = \n= 2- Elmiminar elemento    = \n= 3- Buscar elemento       = \n= 4- Imprimir pila         = \n= 5- Salir                 = \n=                          = \n============================ \nOpciòn:");
+        //Igual resultado; mala estetica en el codigo.
+    }
+    
+}
